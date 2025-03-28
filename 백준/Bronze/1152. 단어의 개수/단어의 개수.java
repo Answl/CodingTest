60da4c59d2b9b74beff8a4cee366af8afc13fcb4
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-03-28 17:46:02 +0900

    [Bronze II] Title: 단어의 개수, Time: 236 ms, Memory: 20108 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,14 +1,12 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = (br.readLine()).split(" ");
-
-        if(str.length==0){
-            System.out.println(str.length);
-        }else{
-            System.out.println((str[0]!="")?str.length:str.length-1);
-        }
+        String str = br.readLine().trim(); //앞뒤 공백 제거
+        
+        StringTokenizer st = new StringTokenizer(str);
+        System.out.println(st.countTokens());
     }
 }
