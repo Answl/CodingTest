542216a2c02268fedb8699f2a8f660bf955dd7fb
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-03-28 16:34:38 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -6,16 +6,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int originScore[] = new int[N];
         
         int sumScore = 0;
         int maxScore = 0;
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i=0;i<N;i++){
-            originScore[i] = Integer.parseInt(st.nextToken());
-            maxScore = Math.max(maxScore, originScore[i]);
-            sumScore += originScore[i];
+            int originScore = Integer.parseInt(st.nextToken());
+            maxScore = Math.max(maxScore, originScore);
+            sumScore += originScore;
         }
 
         double currentScore = (double) sumScore / maxScore * 100 / N;
