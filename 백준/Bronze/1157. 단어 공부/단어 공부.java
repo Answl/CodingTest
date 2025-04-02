b4425911041bf1d6cdf3d710b5d120a7fd748403
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-04-02 11:45:33 +0900

    [Bronze I] Title: 단어 공부, Time: 256 ms, Memory: 23108 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -4,33 +4,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String st = br.readLine().toUpperCase();
         int[] arr_ch = new int[26];
 
         for(char ch : st.toCharArray()){
-            arr_ch[ch-'A'] += 1;
+            arr_ch[ch-'A']++;
         }
 
         int max=0;
-        int maxChar=0;
+        int maxChar=-1;
+        boolean isSeveral = false;
+
         for(int i=0; i<26; i++){
             if(max < arr_ch[i]){
                 max = arr_ch[i];
                 maxChar = i;
-            }
-        }
-
-        boolean isSeveral = false;
-        for(int i=maxChar+1; i<26; i++){
-            if(max == arr_ch[i]){
+                isSeveral = false;
+            }else if(max == arr_ch[i]){
                 isSeveral = true;
-                System.out.println("?");
-                break;
             }
         }
 
-        if(!isSeveral) System.out.println((char)(maxChar+'A'));
+        System.out.println(!isSeveral ? (char)(maxChar+'A') : "?");
     }
 }
