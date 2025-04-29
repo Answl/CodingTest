8a2dcbbaa04a7a332a92d5afd379f9946f5d9b5d
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-04-29 10:21:08 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 444 ms, Memory: 30264 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Main {
     static int[][] arr;
+    static int[][] prefix;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,12 +11,20 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n][m];
+        arr = new int[n+1][m+1];
+        prefix = new int[n+1][m+1];
 
-        for(int i=0;i<n;i++){
+        for(int i=1;i<=n;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<m;j++){
+            for(int j=1;j<=m;j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //누적합 배열
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                prefix[i][j] = prefix[i-1][j] +  prefix[i][j-1] - prefix[i-1][j-1] + arr[i][j];
             }
         }
 
@@ -27,13 +36,8 @@ public class Main {
             int c = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
 
-            int colSum = 0;
-            for(int j=a-1;j<c;j++){
-                for(int x=b-1;x<d;x++){
-                    colSum += arr[j][x];
-                }
-            }
-            System.out.println(colSum);
+            int result = prefix[c][d] - prefix[c][b-1] - prefix[a-1][d] + prefix[a-1][b-1];
+            System.out.println(result);
         }
     }
 }
