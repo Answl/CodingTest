2d4d9707a142c87aefc12d05b22a7aee015c9954
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-05-30 09:25:24 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.26 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
-    static int[] dr = {1, -1, 0, 0};
-    static int[] dc = {0, 0, 1, -1};
+    static int[] dr = {-1, 1, 0,0};
+    static int[] dc = {0, 0, -1, 1};
     static int rowSize, colSize;
     public int solution(int[][] maps) {
         rowSize = maps.length;
@@ -11,15 +11,17 @@ class Solution {
         boolean[][] visited = new boolean[rowSize][colSize];
         q.offer(new int[]{0, 0, 1});
         visited[0][0] = true;
-        
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            if(cur[0] == rowSize-1 && cur[1] == colSize-1) return cur[2];
+            int curR = cur[0], curC = cur[1], step = cur[2];
+            if(curR == rowSize-1 && curC == colSize-1) return step;
             for(int i=0; i<4; i++){
-                int nextR = cur[0] + dr[i], nextC = cur[1] + dc[i];
-                if(isInBound(nextR, nextC) && !visited[nextR][nextC] && maps[nextR][nextC]==1){
-                    q.offer(new int[]{nextR, nextC, cur[2]+1});
-                    visited[nextR][nextC] = true;
+                int nextR = curR + dr[i], nextC = curC + dc[i];
+                if(isInBound(nextR, nextC) && !visited[nextR][nextC]){
+                    if(maps[nextR][nextC] == 1){
+                        q.offer(new int[]{nextR, nextC, step+1});
+                        visited[nextR][nextC] = true;
+                    }
                 }
             }
         }
