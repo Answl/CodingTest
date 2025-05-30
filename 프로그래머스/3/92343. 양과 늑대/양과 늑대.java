9c2a959695a7aea42eeaf93a2ebc10a2e26f3e10
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-05-30 11:13:39 +0900

    [level 3] Title: 양과 늑대, Time: 2.41 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,26 +1,23 @@
 class Solution {
-    
+    static int size;
     public int solution(int[] info, int[][] edges) {
-        int node = info.length;
-        boolean[] visited = new boolean[node];
+        size = info.length;
+        boolean[] visited = new boolean[size];
         visited[0] = true;
-        return dfs(info, edges, visited, 1, 0); //sheep, wolf
+        return dfs(info, edges, visited, 0, 1, 0); //노드, 양
     }
-    private int dfs(int[] info, int[][] edges, boolean[] visited, int sheep, int wolf){
+    private int dfs(int[] info, int[][] edges, boolean[] visited, int node, int sheep, int wolf){
         if(sheep <= wolf) return sheep;
         int maxSheep = sheep;
-        
-        for(int[] edge : edges){    
+        for(int[] edge : edges){
             int parent = edge[0], child = edge[1];
             if(visited[parent] && !visited[child]){
                 visited[child] = true;
-                // 양
-                if(info[child] == 0){
-                    maxSheep = Math.max(maxSheep, dfs(info, edges, visited, sheep+1, wolf));
-                }
-                //늑대
-                else{
-                    maxSheep = Math.max(maxSheep, dfs(info, edges, visited, sheep, wolf+1));
+                //양
+                if(info[child]==0){
+                    maxSheep = Math.max(maxSheep, dfs(info, edges, visited, child, sheep+1, wolf));
+                }else{
+                    maxSheep = Math.max(maxSheep, dfs(info, edges, visited, child, sheep, wolf+1));
                 }
                 visited[child] = false;
             }
