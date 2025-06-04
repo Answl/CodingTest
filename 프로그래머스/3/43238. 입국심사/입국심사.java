a481dcd29f2d3e7ea11ad45789bd80e0f2ef2532
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-06-04 18:06:30 +0900

    [level 3] Title: 입국심사, Time: 67.33 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        long max = 0;
+        for (int time : times){
+            max = Math.max(max, time);
+        }
+        
+        long s = times[0], e = (n / times.length +1) * max, mid;
+        while(s <= e) {
+            mid = (s+e) /2;
+            long sum = 0;
+            for(int time : times) {
+                sum += mid / time;
+            }
+            if(sum >= n) {
+                //줄여
+                e = mid -1;
+                answer = mid;
+            } else if (sum < n) {
+                s = mid + 1;
+            } 
+        }
+        return answer;
+    }
+}
