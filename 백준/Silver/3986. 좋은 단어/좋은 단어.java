e36ed21797a2db408a26ff191717ed06080f25af
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-06-06 16:20:21 +0900

    [Silver IV] Title: 좋은 단어, Time: 264 ms, Memory: 24080 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            Deque<Character> stack = new ArrayDeque<>();
+            char[] chars = br.readLine().toCharArray();
+            if(chars.length % 2 == 0) { //길이가 짝수면 돌리기
+                for (char c : chars) {
+                    //여는거(처음짝) -> 없거나 pop한거랑 다른거면 넣어
+                    if (stack.isEmpty() || stack.peek() != c) {
+                        stack.push(c);
+                    }
+                    //닫는거(두번짝) -> pop한거랑 같으면 빼
+                    else if (!stack.isEmpty() && stack.peek() == c) {
+                        stack.pop();
+                    }
+                }
+                if (stack.isEmpty()) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
