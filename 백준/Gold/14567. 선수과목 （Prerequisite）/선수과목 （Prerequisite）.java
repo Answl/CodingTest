991d288b4b92fdb555b9d3446425e540dfc622db
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-07-24 19:28:13 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 572 ms, Memory: 128508 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] dp;
+    static List<List<Integer>> list = new ArrayList<>();
+    static Queue<int[]> queue;
+    static int[] parentCount;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        dp = new int[N+1];
+        for(int i = 0; i <= N; i++){
+            list.add(new ArrayList<>());
+        }
+
+        parentCount = new int[N+1];
+        //Arrays.fill(parentCount, 1);
+        //boolean[] existParent = new boolean[N+1];
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list.get(x).add(y);
+            parentCount[y] ++;
+            //existParent[y] = true;
+        }
+        //System.out.println(Arrays.toString(dp));
+
+        queue = new ArrayDeque<>();
+        for(int i=1; i<=N; i++){
+            if(parentCount[i] == 0){
+                dp[i] = 1;
+                queue.add(new int[]{i, 1});
+            }
+        }
+        bfs();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            sb.append(dp[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    public static void bfs(){
+//        parentCount[start]--;
+//        System.out.println(Arrays.toString(parentCount));
+//        for(int[] in : queue) {
+//            System.out.println(in[0] + " " + in[1]);
+//        }
+
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int curS = cur[0], curD = cur[1];
+
+            for(Integer l : list.get(curS)){
+                parentCount[l]--;
+                if(parentCount[l] == 0) {
+                    queue.add(new int[]{l, curD + 1});
+
+//                    for(int[] in : queue) {
+//                        System.out.println(in[0] + " " + in[1]);
+//                    }
+//                    System.out.println(Arrays.toString(parentCount));
+                    dp[l] = Math.max(dp[l], curD + 1);
+                }
+            }
+        }
+    }
+}
