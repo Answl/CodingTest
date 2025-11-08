c1ea22ac6b1a672209cea7bed9f1e901cc715f32
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-11-09 02:42:31 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 804 ms, Memory: 280264 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] dr = {0, 0, -1, 1};
+    static int[] dc = {-1, 1, 0, 0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
+        while(true){
+            //반복
+            int N = Integer.parseInt(br.readLine());
+            if(N == 0) break;
+
+            //입력
+            int[][] cave = new int[N][N];
+            int[][] sum = new int[N][N];
+            for(int i=0; i<N; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j=0; j<N; j++){
+                    cave[i][j] = Integer.parseInt(st.nextToken());
+                    sum[i][j] = N*9;
+                }
+            }
+            sum[0][0] = cave[0][0];
+
+            //구현
+            //Queue<int[]> q = new PriorityQueue<>((a,b) -> a[2]-b[2]);
+            Queue<int[]> q = new ArrayDeque<>();
+            q.offer(new int[]{0, 0, cave[0][0]});
+
+            while(!q.isEmpty()){
+                int[] cur = q.poll();
+                int r = cur[0], c = cur[1], w = cur[2];
+//                System.out.println(Arrays.deepToString(sum));
+
+                //종료
+                if(r == N-1 && c == N-1) continue;
+
+                for(int i=0; i<4; i++){
+                    int nr = r + dr[i], nc = c + dc[i];
+                    if(!(nr >= 0 && nr < N && nc >= 0 && nc < N)) continue;
+                    
+                    int nw = w + cave[nr][nc];
+                    if(sum[nr][nc] <= nw) continue;
+
+//                    System.out.print(nr + " " + nc + " " + (w+cave[nr][nc]));
+//                    System.out.println();
+                    sum[nr][nc] = nw;
+                    q.offer(new int[]{nr, nc, sum[nr][nc]});
+                }
+            }
+            sb.append("Problem " + count++ + ": " + sum[N-1][N-1]).append("\n");
+//            System.out.println(sb);
+        }
+        System.out.println(sb);
+    }
+}
