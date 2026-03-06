6ff5385d74afe16996edf7306f3582a09bc4e8d7
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2026-03-06 23:49:11 +0900

    [Gold I] Title: 최솟값 찾기, Time: 1956 ms, Memory: 629296 KB -BaekjoonHub

---
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Deque<int[]> q = new ArrayDeque<>(); //idx, 값 저장
+
+        int left = -L+1, right = 0;
+        while(right<N){
+            // 내가 맨 마지막 값이 되도록 offer
+            while(true){
+                if(q.isEmpty() || q.peekLast()[1]<num[right]){
+                        q.offer(new int[]{right, num[right]});
+                        break;
+                } else {
+                        q.pollLast();
+                }
+            }
+
+            //맨 앞 값이 범위를 벗어나는지 확인
+            if(q.peek()[0]<left) q.poll();
+
+            //맨 앞 값(최소값) print
+            sb.append(q.peek()[1]).append(" ");
+
+            //다음 윈도우로
+            left++; right++;
+        }
+
+        System.out.println(sb);
+    }
+}
