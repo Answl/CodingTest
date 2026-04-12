e25f9b9b8555f5f94b2c426951b0a7f38e65ef54
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2026-04-13 00:00:39 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18336 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int[] dp = new int[x+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[1] = 0;
+
+        for(int i=2; i<=x; i++){
+            if(i%3==0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
+            if(i%2==0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
+            dp[i] = Math.min(dp[i-1]+1, dp[i]);
+        }
+
+        System.out.println(dp[x]);
+    }
+}
