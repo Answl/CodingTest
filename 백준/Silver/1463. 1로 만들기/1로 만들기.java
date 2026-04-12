459a9ae41b6bbf786fba33229055901c1cc62d3f
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2026-04-13 00:08:56 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18320 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,23 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int min;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
+        /**
+         * 2 -> ㅃㅐ기1
+         * 3 -> 3으로 나누기
+         * 4 -> 나누기 2
+         * 5 -> (-1) 4 나누기 2
+         * 6 -> 나누기2 하고 나누기3 아니면 나누기3 하고 빼기1
+         */
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int x = Integer.parseInt(br.readLine());
         int[] dp = new int[x+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[1] = 0;
 
-        for(int i=2; i<=x; i++){
-            if(i%3==0) dp[i] = Math.min(dp[i/3]+1, dp[i]);
-            if(i%2==0) dp[i] = Math.min(dp[i/2]+1, dp[i]);
-            dp[i] = Math.min(dp[i-1]+1, dp[i]);
+        min = Integer.MAX_VALUE;
+        dfs(x, 0);
+
+        System.out.println(min);
+    }
+
+    private static void dfs(int n, int count) {
+        if (count >= min) return;
+
+        if (n == 2) {
+            min = Math.min(min, count+1);
+            //System.out.println("min="+ min);
+            return;
         }
 
-        System.out.println(dp[x]);
+        if (n == 1) {
+            min = Math.min(min, count);
+            //System.out.println("min="+ min);
+            return;
+        }
+
+        if (n % 3 == 0) {
+            //System.out.println("%3");
+            dfs(n/3, count+1);
+        }
+
+
+        if (n % 2 == 0) {
+            //System.out.println("%2");
+            dfs(n/2, count+1);
+        }
+
+        //System.out.println("-1");
+        dfs(n - 1, count+1);
     }
 }
