b2a9c97fa6465ae0da1457a6678686eaa3c433e7
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2026-05-18 15:31:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.22 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,33 +1,38 @@
 import java.util.*;
+
 class Solution {
-    static int[] dr = {-1, 1, 0,0};
-    static int[] dc = {0, 0, -1, 1};
-    static int rowSize, colSize;
+    public int[] dr = {-1,0,0,1};
+    public int[] dc = {0,1,-1,0};
+    
     public int solution(int[][] maps) {
-        rowSize = maps.length;
-        colSize = maps[0].length;
+        int answer = 0;
         
-        Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[rowSize][colSize];
-        q.offer(new int[]{0, 0, 1});
-        visited[0][0] = true;
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int curR = cur[0], curC = cur[1], step = cur[2];
-            if(curR == rowSize-1 && curC == colSize-1) return step;
+        int m = maps.length;
+        int n = maps[0].length;
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0,0,1}); //r,c,값
+        
+        boolean[][] visited = new boolean[m][n];
+        
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            
+            if(cur[0] == m-1 && cur[1] == n-1) return cur[2];
+            
             for(int i=0; i<4; i++){
-                int nextR = curR + dr[i], nextC = curC + dc[i];
-                if(isInBound(nextR, nextC) && !visited[nextR][nextC]){
-                    if(maps[nextR][nextC] == 1){
-                        q.offer(new int[]{nextR, nextC, step+1});
-                        visited[nextR][nextC] = true;
+                int nr = cur[0] + dr[i];
+                int nc = cur[1] + dc[i];
+                
+                if(0<=nr && nr < m && 0<=nc && nc < n && !visited[nr][nc]){
+                    if(maps[nr][nc] != 0){
+                        queue.add(new int[]{nr, nc, cur[2]+1});
+                        visited[nr][nc] = true;
                     }
                 }
             }
         }
+        
         return -1;
-    }
-    private boolean isInBound(int r, int c){
-        return 0<=r && r<rowSize && 0<=c && c<colSize;
     }
 }
