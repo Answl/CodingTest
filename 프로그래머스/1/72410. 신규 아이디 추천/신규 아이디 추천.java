bcfea258cccbadd63753f933e83a4b2e22c75edf
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2026-09-19 21:41:42 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.02 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,25 +1,31 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
-        //1
-        answer = new_id.toLowerCase();
-        //2
-        answer = answer.replaceAll("[^a-z0-9-_.]", "");
-        //3
+        
+        //1. 소문자로
+        String answer = new_id.toLowerCase();
+        
+        //2. 특수문자 제거
+        answer = answer.replaceAll("[^a-zA-Z0-9\\-_.]",  "");
+        
+        //3. 마침표2 -> 1
         answer = answer.replaceAll("\\.{2,}", ".");
-        //4
-        answer = answer.replaceAll("^\\.|\\.$", "");
-        //5
-        if(answer.isEmpty()) answer = "a";
-        //6
-        if(answer.length()>=16) {
-            answer = answer.substring(0,15);
+        
+        //4. 처음/끝 . 삭제
+        if(answer.length()>0 && answer.charAt(0) == '.') answer = answer.substring(1);
+        if(answer.length()>0 && answer.charAt(answer.length()-1) == '.') answer = answer.substring(0, answer.length()-1);
+        
+        //5. 빈문자열  -> a
+        if(answer.length()==0) answer = "a";
+        
+        //6. 16자이상 -> 제거(15자까지)
+        if(answer.length()>=16) answer = answer.substring(0,15);
+        if(answer.charAt(answer.length()-1) == '.') answer = answer.substring(0,answer.length()-1);
+        
+        //7. 2자이하 -> 마지막 글자 반복
+        if(answer.length()<=2){
+            while(answer.length() < 3) answer += String.valueOf(answer.charAt(answer.length()-1));
         }
-        answer = answer.replaceAll("\\.$", "");
-        //7
-        while(answer.length() <3){
-            answer += answer.substring(answer.length()-1, answer.length());
-        }
+        
         return answer;
     }
 }
