e307edbffb44621c434a05ede79c2d9a362edac1
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2026-09-20 14:14:12 +0900

    [level 3] Title: 네트워크, Time: 0.57 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,44 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n];
-        Queue<Integer> queue = new ArrayDeque<>();
         
-        for(int j=0; j<n; j++){
-            if(visited[j]) continue;
-            
-            visited[j] = true;
-            queue.add(j);
-            
-            while(!queue.isEmpty()){
-            int cur = queue.poll();
-            for(int i=0; i<n; i++){
-                if(i == cur) continue;
-                if(computers[cur][i]==1 && !visited[i]) {
-                    queue.add(i);
-                    visited[i] = true;
-                }
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            list.add(new ArrayList<>());
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                 if(computers[i][j] == 1) list.get(i).add(j);
             }
         }
-        answer ++;
+        //System.out.println(list);
+        
+        //bfs
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n];
+        
+        for(int i=0; i<n; i++){
+            //System.out.println(Arrays.toString(visited));
+            if(!visited[i]) {
+                queue.offer(i);
+                visited[i] = true;
+                answer ++;
+                //System.out.println(i);
+            while(!queue.isEmpty()){
+            int cur = queue.poll();
+            //System.out.println(cur + "" + list.get(cur));
+            for(int l : list.get(cur)){
+                //System.out.println(l + " "+ queue);
+                if(visited[l]) continue;
+                //System.out.println("넣자");
+                queue.offer(l);
+                visited[l] = true;
+            }
+            }
+        }
         }
         
         return answer;
